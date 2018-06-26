33ef0c85b28780e3b5b4669c0bf511abe43f4a12
ROHIT POTTER <rohitpotter12@gmail.com>
2018-06-26 14:54:28 +0530

    updating files

--- src/home.php
+++ src/home.php
@@ -1,7 +1,7 @@
 <?php
 $servername="localhost";
 $username="root";
-$password="";
+$password="rohit@1998";
 $dbname="atm";
 $conn=new mysqli($servername,$username,$password,$dbname);
 if($conn->connect_error)
